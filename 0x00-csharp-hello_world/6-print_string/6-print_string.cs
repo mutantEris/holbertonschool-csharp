b1821c8f8eea11cd93979d75ec856f5e69fb7096
mutantEris <mmires@live.com>
2022-02-01 23:43:21 -0600

    files

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{0}{0}", str);
+        Console.WriteLine("{0}", str.Substring(0, 9));
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+    }
+}
