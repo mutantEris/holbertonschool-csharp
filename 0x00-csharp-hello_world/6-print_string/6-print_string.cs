9343230635d9ac0eda0f393228dc35f2a971388f
mutantEris <mmires@live.com>
2022-02-01 23:51:27 -0600

    fixes

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -7,6 +7,5 @@ class Program
 		string str = "Holberton School";
 		Console.WriteLine("{0}{0}{0}", str);
         Console.WriteLine("{0}", str.Substring(0, 9));
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
     }
 }
